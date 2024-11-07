2118a8fba0534ea5b80f77e67e649d3cb78c6358
tangyixu <tangyixu@grinnell.edu>
2024-11-06 23:16:40 -0600

    Set up of the game

--- src/main/java/edu/grinnell/csc207/sample/Game.java
+++ src/main/java/edu/grinnell/csc207/sample/Game.java
@@ -4,6 +4,7 @@ import edu.grinnell.csc207.util.ArrayUtils;
 import edu.grinnell.csc207.util.IOUtils;
 import edu.grinnell.csc207.util.Matrix;
 import edu.grinnell.csc207.util.MatrixV0;
+import edu.grinnell.csc207.util.FourInRowV0;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -14,6 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+
 /**
  * A two-player game with the goal of connecting 4 tokens in a line.
  * 
@@ -21,19 +23,6 @@ import java.util.Random;
  * @author Tiffany Tang
  */
 public class Game {
-  // +-----------+---------------------------------------------------
-  // | Constants |
-  // +-----------+
-
-  /**
-   * The default width.
-   */
-  static final int DEFAULT_WIDTH = 10;
-
-  /**
-   * The default number of rows.
-   */
-  static final int DEFAULT_HEIGHT = 10;
 
   // +----------------+----------------------------------------------
   // | Helper methods |
@@ -51,8 +40,8 @@ public class Game {
 
                 Command-line arguments:
 
-                * -w width - set up the width of the board
-                * -h height - set up the height of the board
+                * -r rows - set up the number of rows of the board
+                * -c cols  - set up the number of columns of the board
                 * -p1 player1 - set up the name of the player one
                 * -p2 player2 - set up the name of the player two
 
@@ -94,52 +83,46 @@ public class Game {
   public static void main(String[] args) throws IOException {
     PrintWriter pen = new PrintWriter(System.out, true);
     BufferedReader eyes = new BufferedReader(new InputStreamReader(System.in));
-
-    int rrRemaining = 2;
-    int rcRemaining = 2;
-    int icRemaining = 2;
-    int irRemaining = 2;
-    int width = DEFAULT_WIDTH;
-    int height = DEFAULT_HEIGHT;
-    Random rand = new Random();
-    int game = rand.nextInt();
+    int rows = 0;
+    int cols = 0;
+    String player1;
+    String player2;
 
     // Process the command line
     for (int i = 0; i < args.length; i++) {
       switch (args[i]) {
-        case "-w":
+        case "-r":
           try {
-            width = Integer.parseInt(args[++i]);
-          } catch (Exception e) {
-            System.err.printf("Invalid width: %s (not an integer)\n", args[i]);
-            return;
-          } // try/catch
-          if (width < 4) {
-            System.err.printf("Invalid width: %s (less than 4)\n", width);
-            return;
-          } // if
-          break;
-
-        case "-h":
-          try {
-            height = Integer.parseInt(args[++i]);
+            rows = Integer.parseInt(args[++i]);
           } catch (Exception e) {
             System.err.printf("Invalid height: %s (not an integer)\n", args[i]);
             return;
           } // try/catch
-          if (height < 4) {
-            System.err.printf("Invalid height: %s (less than 4)\n", height);
+          if (rows < 6) {
+            System.err.printf("Invalid height: %s (less than 6)\n", rows);
             return;
           } // if
           break;
 
-        case "-s":
+        case "-c":
           try {
-            game = Integer.parseInt(args[++i]);
+            cols = Integer.parseInt(args[++i]);
           } catch (Exception e) {
-            System.err.printf("Invalid game number: %s\n", args[i]);
+            System.err.printf("Invalid height: %s (not an integer)\n", args[i]);
             return;
           } // try/catch
+          if (cols < 7) {
+            System.err.printf("Invalid height: %s (less than 7)\n", cols);
+            return;
+          } // if
+          break;
+
+        case "-p1":
+          player1 = args[++i];
+          break;
+
+        case "-p2":
+          player2 = args[++i];
           break;
 
         default:
@@ -150,114 +133,16 @@ public class Game {
 
     // Get started
     printInstructions(pen);
-    pen.print("Hit return to continue");
+    pen.print("Hit return to start the game");
     pen.flush();
     eyes.readLine();
 
     // Set up the board
-    Matrix<String> board = setupBoard(width, height, game);
+    FourInRowV0 sampleFIR = new FourInRowV0(pen,rows,cols);
+    Matrix<String> board = sampleFIR.board();
 
     // Run the game
-    pen.println("Game setup number " + game);
-    pen.println();
-
-    String[] commands = new String[] {"RR", "RC", "IR", "IC", "SKIP", "UNDO", "DONE"};
-    boolean done = false;
-    Matrix<String> prev = board.clone();
-    while (!done) {
-      Matrix.print(pen, board, true);
-      String command = IOUtils.readCommand(pen, eyes, "Action: ", commands);
-      switch (command.toUpperCase()) {
-        case "RR":
-          if (rrRemaining < 0) {
-            pen.println("Sorry, you've used up your remove row commands.");
-            break;
-          } // if
-          --rrRemaining;
-          if (rrRemaining <= 0) {
-            commands = ArrayUtils.removeAll(commands, "RR");
-          } // if
-          int rowToRemove =
-              IOUtils.readInt(pen, eyes, "Row: ", 0, board.height());
-          prev = board.clone();
-          board.deleteRow(rowToRemove);
-          process(board);
-          break;
-
-        case "RC":
-          if (rcRemaining < 0) {
-            pen.println("Sorry, you've used up your remove column commands.");
-            break;
-          } // if
-          --rcRemaining;
-          if (rcRemaining <= 0) {
-            commands = ArrayUtils.removeAll(commands, "RC");
-          } // if
-          int colToRemove =
-              IOUtils.readInt(pen, eyes, "Column: ", 0, board.width());
-          prev = board.clone();
-          board.deleteCol(colToRemove);
-          process(board);
-          break;
-
-        case "IR":
-          if (irRemaining < 0) {
-            pen.println("Sorry, you've used up your insert row commands.");
-            break;
-          } // if
-          --irRemaining;
-          if (irRemaining <= 0) {
-            commands = ArrayUtils.removeAll(commands, "IR");
-          } // if
-          int rowToInsert =
-              IOUtils.readInt(pen, eyes, "Row: ", 0, board.height() + 1);
-          prev = board.clone();
-          board.insertRow(rowToInsert);
-          process(board);
-          break;
-
-        case "IC":
-          if (icRemaining < 0) {
-            pen.println("Sorry, you've used up your insert row commands.");
-            break;
-          } // if
-          --icRemaining;
-          if (icRemaining <= 0) {
-            commands = ArrayUtils.removeAll(commands, "IC");
-          } // if
-          int colToInsert =
-              IOUtils.readInt(pen, eyes, "Column: ", 0, board.width() + 1);
-          prev = board.clone();
-          board.insertCol(colToInsert);
-          process(board);
-          break;
-
-        case "DONE":
-          done = true;
-          break;
-
-        case "SKIP":
-          prev = board.clone();
-          process(board);
-          break;
-
-        case "UNDO":
-          if (board == prev) {
-            pen.println("Sorry: There's only one level of undo.");
-          } else {
-            board = prev;
-          } // if/else
-          break;
-
-        default:
-          pen.printf("Unexpected command: '%s'. Please try again.\n", command);
-          break;
-      } // switch
-    } // while
-
-    // Print final results
-    printResults(pen, board);
-
+    
     // And we're done
     pen.close();
   } // main(String[])
